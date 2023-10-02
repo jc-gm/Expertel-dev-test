64f67f0d0ce1385aca9fd867847da3310b340ddc
Juan <juan@pulpandfiber.com>
2023-10-02 19:54:57 -0400

    doc update

--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -24,7 +24,19 @@ class MeetingController extends Controller
     }
 
 
-    //modify in any way you want
+    /*
+        The input is a JSON-object like this one:
+
+        {
+            "meeting_name": "Meeting Name",
+            "start_time": "2023-10-29 20:30:00",
+            "end_time": "2023-10-29 21:30:00",
+            "users": [1]
+        }
+
+        You can modify this function in any way you want
+
+    */
     public function create(Request $request)
     {
         $service = new MeetingService;
